7b2d34d74557db6cf9c616b40a4bcfd3f0bbe328
hehejow <sebastianpape1@gmail.com>
2023-08-13 21:03:08 +0200

    finals

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,18 +1,23 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    let formText = document.getElementById('name').value;
+    Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-        .then(res => {
-            return res.json()
-        })
+    console.log("::: Form Submitted :::");
+    fetch(`https://api.api-ninjas.com/v1/animals?name=${formText}`, {
+        headers: {
+            'X-Api-Key': 'KyPZNOaKqVwPjgqUQJEcyQ==tdazg35srPJ1jk5O'
+        }
+    })
+        .then(res => res.json())
         .then(function (data) {
-            document.getElementById('results').innerHTML = data.message
+            console.log(data); // Log the data object to inspect its structure
+            const locations = data[0].locations;
+            document.getElementById('results').innerHTML = data[0].locations.join(', ');
         })
+        .catch(error => console.error('Request failed:', error));
 }
 
 export { handleSubmit }
